ba7f6af5451ce5dac06e2bb861bf31cbd6874fea
Andrey Drobakh <drobahandrey7@gmail.com>
2026-06-17 15:18:40 +0500

    Added obstacles:
    - added cactus scene with Cactus Area2D root node;
    - instantiated cactus scene into main scene;
    - added connection methods for signals in coin and powerup
    scenes, so that when these nodes appear and collide with
    an obstacle, they change position;

--- powerup.gd
+++ powerup.gd
@@ -16,3 +16,9 @@ func disappear() :
 
 func _on_lifetimer_timeout() -> void:
 	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group( "obstacles" ) :
+		position = Vector2( randi_range( 0, screensize.x ), randi_range( 0, screensize.y ) )
+
